cf20f1d0bf95d8658c88647511e8909a10a1e1b9
MinhQuan1912 <nguyenquan19122002@gmail.com>
2026-08-01 08:33:17 +0700

    fix build

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
   app.enableCors({ origin: origins, credentials: true });
 
 
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT ?? 3001, '0.0.0.0');
 }
 bootstrap();
